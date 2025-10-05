4addc540ae9866d2afdad13b3d43be43d9b73e7d
Karel <karelriyan@gmail.com>
2025-10-06 03:04:40 +0700

    Fix: Daftarkan Profile dan RiwayatAktivitas ke AndroidManifest.xml

--- app/src/main/java/com/main/cipta_muri_mobile/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/main/cipta_muri_mobile/ui/profile/ProfileActivity.kt
@@ -1,4 +1,12 @@
 package com.main.cipta_muri_mobile.ui.profile
 
-class ProfileActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.main.cipta_muri_mobile.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
 }
