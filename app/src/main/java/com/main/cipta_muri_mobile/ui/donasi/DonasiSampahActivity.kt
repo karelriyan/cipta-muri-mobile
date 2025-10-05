ffeb439c219f52ff0c36e1aa9b6a115a786be6f3
Karel <karelriyan@gmail.com>
2025-10-06 03:55:07 +0700

    Feat: Button Back Complete

--- app/src/main/java/com/main/cipta_muri_mobile/ui/donasi/DonasiSampahActivity.kt
+++ app/src/main/java/com/main/cipta_muri_mobile/ui/donasi/DonasiSampahActivity.kt
@@ -3,10 +3,32 @@ package com.main.cipta_muri_mobile.ui.donasi
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.main.cipta_muri_mobile.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.content.Intent
+import android.widget.TextView
+import androidx.activity.viewModels
+import com.main.cipta_muri_mobile.databinding.ActivityDonasiSampahBinding
+import com.main.cipta_muri_mobile.ui.main.MainActivity
 
 class DonasiSampahActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDonasiSampahBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_donasi_sampah)
+        binding = ActivityDonasiSampahBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+        // Back
+        binding.btnBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
 }
--- app/src/main/java/com/main/cipta_muri_mobile/ui/saldo/riwayat/RiwayatPenarikanActivity.kt
+++ app/src/main/java/com/main/cipta_muri_mobile/ui/saldo/riwayat/RiwayatPenarikanActivity.kt
@@ -3,10 +3,29 @@ package com.main.cipta_muri_mobile.ui.saldo.riwayat
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.main.cipta_muri_mobile.R
+import android.content.Intent
+import android.widget.TextView
+import androidx.activity.viewModels
+import com.main.cipta_muri_mobile.databinding.ActivityRiwayatPenarikanBinding
+import com.main.cipta_muri_mobile.ui.main.MainActivity
 
 class RiwayatPenarikanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRiwayatPenarikanBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_riwayat_penarikan)
+        binding = ActivityRiwayatPenarikanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+        // Back
+        binding.btnBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
 }
