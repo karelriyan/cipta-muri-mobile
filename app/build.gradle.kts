533cb68c4599f1a63fc4833bc8ddea25e9db6fb1
Karel <karelriyan@gmail.com>
2025-10-06 15:16:59 +0700

    Feat: Leaderboard Layout (kurang rapi dikit)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
