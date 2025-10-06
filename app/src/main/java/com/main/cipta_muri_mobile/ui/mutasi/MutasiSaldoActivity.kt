28ad72301165cc91bc722c0f3b882e3e2ebd2289
Karel <karelriyan@gmail.com>
2025-10-06 10:10:37 +0700

    Fix: Navigasi Card

--- app/src/main/java/com/main/cipta_muri_mobile/ui/mutasi/MutasiSaldoActivity.kt
+++ app/src/main/java/com/main/cipta_muri_mobile/ui/mutasi/MutasiSaldoActivity.kt
@@ -1,4 +1,31 @@
 package com.main.cipta_muri_mobile.ui.mutasi
 
-class MutasiSaldoActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.main.cipta_muri_mobile.R
+import android.content.Intent
+import android.widget.TextView
+import androidx.activity.viewModels
+import com.main.cipta_muri_mobile.databinding.ActivityMutasiSaldoBinding
+import com.main.cipta_muri_mobile.ui.main.MainActivity
+
+class MutasiSaldoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMutasiSaldoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMutasiSaldoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        // Back
+        binding.btnBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
 }
